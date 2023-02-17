1b88511c6caa5343940cbc47193e3f6392f03943
Sophie Strausberg <sophie.strausberg@gmail.com>
2023-02-17 10:50:14 -0500

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Sophie Strausberg on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
